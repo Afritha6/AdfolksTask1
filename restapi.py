923643f66682027f6590b2e4888a16042a5de91d
Afritha <afrithas1@gmail.com>
2021-08-20 12:17:00 +0530

    To create new topic

--- restapi.py
+++ restapi.py
@@ -12,19 +12,34 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 class Topic(db.Model):
-    topic_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     topic_name = db.Column(db.String(200))
     commment = db.Column(db.String(300))
 
 # create db schema class
 class TopicSchema(ma.Schema):
     class Meta:
-        fields = ('topic_id', 'topic_name', 'commment')
+        fields = ('id', 'topic_name', 'commment')
 
 
 # instantiate schema objects for todolist and todolists
 topic_schema = TopicSchema(many=False)
 topic_schemas = TopicSchema(many=True)
+
+@app.route("/topic", methods = ["POST"])
+def add_topic():
+    try:
+        topic_name = request.json['topic_name']
+
+        new_topic = Topic(topic_name=topic_name)
+        
+        db.session.add(new_topic)
+        db.session.commit()
+
+        return topic_schema.jsonify(new_topic)
+    except Exception as e:
+        return jsonify({"Error 409": "Invalid Request, please try again."})
+        
 
 
 if __name__ == "__main__":
