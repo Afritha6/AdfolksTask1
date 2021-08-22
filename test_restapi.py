b1d8a5aeb74694c4856189ccc497d025f347db3f
Afritha <afrithas1@gmail.com>
2021-08-22 06:01:58 +0530

    test specific topic

--- test_restapi.py
+++ test_restapi.py
@@ -5,11 +5,9 @@ import requests
 class TestAPI(unittest.TestCase):
     URL = "http://127.0.0.1:5000/topic"
 
-    data = {
-        "commment": "AWS is a public cloud provider",
-        "id": 3,
-        "topic_name": "AWS"
-    }
+    data = {'commment': None, 'id': 9, 'topic_name': 'dummydata2'}
+
+    expected_result =  {'commment': None, 'id': 9, 'topic_name': 'dummydata2'}
 
 
     def test_1_get_all_topics(self):
@@ -25,10 +23,17 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(resp.status_code,200)
         print("Test 2 completed")
 
+    def test_3_get_specific_topic(self):
+        resp = requests.get(self.URL + '/9')
+        self.assertEqual(resp.status_code,200)
+        self.assertDictEqual(resp.json() , self.expected_result)
+        print("Test 3 completed")
+
 
 if __name__=="__main__":
     tester = TestAPI()
 
     tester.test_1_get_all_topics()
     tester.test_2_post_topic()
+    tester.test_3_get_specific_topic()
 
