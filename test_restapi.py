0426e7101018c74c363b774237e7a8779350c07d
Afritha <afrithas1@gmail.com>
2021-08-22 05:50:34 +0530

    added test post

--- test_restapi.py
+++ test_restapi.py
@@ -5,6 +5,12 @@ import requests
 class TestAPI(unittest.TestCase):
     URL = "http://127.0.0.1:5000/topic"
 
+    data = {
+        "commment": "AWS is a public cloud provider",
+        "id": 3,
+        "topic_name": "AWS"
+    }
+
 
     def test_1_get_all_topics(self):
         resp = requests.get(self.URL)
@@ -14,8 +20,15 @@ class TestAPI(unittest.TestCase):
 
     
 
+    def test_2_post_topic(self):
+        resp = requests.post(self.URL,json=self.data)
+        self.assertEqual(resp.status_code,200)
+        print("Test 2 completed")
+
+
 if __name__=="__main__":
     tester = TestAPI()
 
-    tester.test_1_get_all_todos()
+    tester.test_1_get_all_topics()
+    tester.test_2_post_topic()
 
